1f3c611ce979267556ea889f98f7f934a2ea157f
Sterling01 <Sterling01git config --global user.email stalinsant1hotmail.com>
2020-06-04 11:53:36 -0500

    Final

--- FigueroaSacoto_Stalin_ExamenMPI.py
+++ FigueroaSacoto_Stalin_ExamenMPI.py
@@ -1,9 +1,167 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun  4 10:08:22 2020
+import time
+from mpi4py import MPI
+import random
 
-@author: usuario
-"""
+'''
+def how_many_max_values_sequential(ar):
+    #find max value of the list
+    maxValue = 0
+    for i in range(len(ar)):
+        if i == 0:
+            maxValue = ar[i]
+        else:
+            if ar[i] > maxValue:
+                maxValue = ar[i]
+    #find how many max values are in the list
+    contValue = 0
+    for i in range(len(ar)):
+        if ar[i] == maxValue:
+            contValue += 1
+    return contValue
+
+MatrizFinal = [] 
+
+ar_count = 100
+#ar_count = 40000000
+
+#Generate ar_count random numbers between 1 and 30
+ar = [random.randrange(1,30) for i in range(ar_count)]
+print(ar)
+#print(ar[0:10])
+
+inicioSec = time.time()
+resultSec = how_many_max_values_sequential(ar)
+finSec =  time.time()
+
+print('Sequential Process took %.3f ms with %d items\n' % ((finSec - inicioSec)*1000, ar_count))
+'''
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+matrizMayores = [] 
+
+start = time.time()
 
 
+if rank == 0:   
+    maxValor=0
+    #ar_count = 100
+    ar_count = 40000000
+    #Generate ar_count random numbers between 1 and 30
+    ar = [random.randrange(1,30) for i in range(ar_count)]
+    print(ar)
+    #print(ar[0:10])
+    
+    ac=0
+    cant=int(len(ar)/(size-1))
+    for dest in range(1,size):
+        comm.send(ar[ac:int(ac+cant)],dest)
+        ac+=int(cant)
+    
+    for i in range(1,size):
+        mayorValor=comm.recv(source=i)
+        matrizMayores.append(mayorValor)
+        #print(extracto)
+    for i in range(len(matrizMayores)):
+        if i == 0:
+            maxValor = matrizMayores[i]
+        else:
+            if matrizMayores[i] > maxValor:
+                maxValor = matrizMayores[i]
+    #find how many max values are in the list
+    contValue = 0
+    for i in range(len(ar)):
+        if ar[i] == maxValor:
+            contValue += 1
+    print(contValue)
+    
+    
+if rank > 0:
+    #print("Proceso ",rank) 
+    data=comm.recv(source=0)
+    print(data)
+    maxValue = 0
+    for i in range(len(data)):
+        if i == 0:
+            maxValue = data[i]
+        else:
+            if data[i] > maxValue:
+                maxValue = data[i]
+    print(maxValue)
+    comm.send(maxValue,0)
+    #print(c)
+
+
+end = time.time()
+'''
+print("Tiempo transcurrido: ",end - start)
+'''
+'''
+    print(offse)
+    print("\n")
+    print(row)
+    print("\n")
+    print(data)
+    print("\n")
+    print(data2)
+    print("\n")
+    '''
+
+
+'''
+    print(averow)
+    print("\n")
+    print(extra)
+'''    
+    
+
+'''
+    print(Matriz)
+    print("\n")
+    print(Matriz[0:2])#Matriz desde la fila 0 hasta la 1     
+
+for i in range(size-1):
+        comm.send(Matriz, i+1)
+        comm.send(Matriz2, i+1)
+           
+else:
+   Matriz = None
+   
+if rank>0:
+    data=comm.recv(source=0)
+    data2=comm.recv(source=0)
+    print(data)
+    print("\n")
+    print(data2)
+    print("\n")
+    
+#print("Proceso: %s" %rank+ " recibe el dato: %d" %data)
+#print(Matriz)
+
+
+
+'''
+
+
+
+
+
+'''
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+
+if rank == 0:
+   array_to_share = [1, 2, 3, 4 ,5 ,6 ,7, 8 ,9 ,10] 
+           
+else:
+   array_to_share = None
+
+recvbuf = comm.scatter(array_to_share, root=0)
+print("process = %d" % rank + " variable shared  = %d " % recvbuf )
+'''
--- FigueroaSacoto_Stalin_ExamenMultiprocessing.py
+++ FigueroaSacoto_Stalin_ExamenMultiprocessing.py
@@ -11,6 +11,7 @@ def how_many_max_values_sequential(ar):
         else:
             if ar[i] > maxValue:
                 maxValue = ar[i]
+    print("Numero mayor: ",maxValue)
     #find how many max values are in the list
     contValue = 0
     for i in range(len(ar)):
@@ -21,9 +22,69 @@ def how_many_max_values_sequential(ar):
  
 
 # Complete the how_many_max_values_parallel function below.
-def how_many_max_values_parallel(ar):
+class productor(multiprocessing.Process, object):
+    #Recibir el parametro con el nombre
+    def __init__(self, queue, matriz):
+        multiprocessing.Process.__init__(self)
+        #Asignar dentro de self
+        self.matriz=matriz
+        self.queue = queue
+        
+        
+    def run(self):
+        ac=0
+        cant=len(self.matriz)/10
+        for i in range(0,10):
+            #Agregar el arreglo en porciones a la cola
+            self.queue.put(self.matriz[ac:int(ac+cant)])
+            #print(self.matriz[ac:int(ac+cant)])
+            ac+=int(cant)
+     
+class consumidor(multiprocessing.Process):
+        
+    def __init__(self, queue):
+        multiprocessing.Process.__init__(self)
+        self.queue = queue
+        
+    def run(self):
+        matMayor=[]
+        while True:
+            if(self.queue.empty()):
+                #print("La cola esta vacia")
+                break
+            else:
+                #Consumir lo que este en la cola en orden FIFO
+                item=self.queue.get()
+                resultado=getMaxValue(item)
+                matMayor.append(resultado)
+        #print(matMayor)
+        mayorGeneral=getMaxValue(matMayor)
+        self.queue.put(mayorGeneral)
+         
+class final(multiprocessing.Process, object):
+    #Recibir el parametro con el nombre
+    def __init__(self, queue, matrizFinal):
+        multiprocessing.Process.__init__(self)
+        #Asignar dentro de self
+        self.matriz=matrizFinal
+        self.queue = queue
+        
+        
+    def run(self):
+        maximo=self.queue.get()
+        cont=how_many_max_values_parallel(self.matriz,maximo)
+        print("Paralelo: ",cont)
+        
+def how_many_max_values_parallel(ar,maximo):
+    maxValue = maximo
+    #find how many max values are in the list
+    contValue = 0
+    for i in range(len(ar)):
+        if ar[i] == maxValue:
+            contValue += 1
+    return contValue
     
-    '''
+def getMaxValue(ar):
     maxValue = 0
     for i in range(len(ar)):
         if i == 0:
@@ -31,48 +92,40 @@ def how_many_max_values_parallel(ar):
         else:
             if ar[i] > maxValue:
                 maxValue = ar[i]
-    #find how many max values are in the list
-    contValue = 0
-    for i in range(len(ar)):
-        if ar[i] == maxValue:
-            contValue += 1
-    return contValue
-    '''
-    return 0
+    return maxValue
+
+
  
 
 if __name__ == '__main__':
-    ar_count = 100
-    #ar_count = 40000000
+    #ar_count = 100
+    ar_count = 40000000
 
     #Generate ar_count random numbers between 1 and 30
     ar = [random.randrange(1,30) for i in range(ar_count)]
     print(ar)
-    print(ar[0:10])
+    #print(ar[0:10])
     
-    pool = multiprocessing.Pool(processes=10)
-    cant = (ar_count/10)
-    acum=0
-    for i in range(0,10):
-        print("Desde ",acum, "hasta ", acum+cant)
-        acum+=cant
-        #pool_outputs = pool.map(how_many_max_values_parallel, ar[])
-        
-    #pool.close() 
-    #pool.join()      
-    
-    #print ('Pool    :', pool_outputs)    
-
     inicioSec = time.time()
     resultSec = how_many_max_values_sequential(ar)
+    print("Secuencial: ",resultSec)
     finSec =  time.time()
-
-    inicioPar = time.time()   
-    resultPar = how_many_max_values_parallel(ar)
-    finPar = time.time()     
-
-    print('Results are correct!\n' if resultSec == resultPar else 'Results are incorrect!\n')
+    
+    
+    inicioPar = time.time()       
+    queue=multiprocessing.Queue()
+    #Enviar una matriz a un proceso
+    procesoProductor = productor(queue,ar) 
+    procesoConsumidor = consumidor(queue)
+    procesoProductor.start()
+    procesoConsumidor.start()
+    procesoProductor.join()
+    procesoConsumidor.join()
+    finPar = time.time() 
+    
+    procesoFinal= final(queue, ar)
+    procesoFinal.start()
+    procesoFinal.join()
 
     print('Sequential Process took %.3f ms with %d items\n' % ((finSec - inicioSec)*1000, ar_count))
-
     print('Parallel Process took %.3f ms with %d items\n' % ((finPar - inicioPar)*1000, ar_count))
